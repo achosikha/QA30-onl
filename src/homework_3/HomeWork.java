e8c5fe499e381b21225d2744c1c427367b4a8936
achosikha <softdevelopment@yahoo.com>
2025-03-05 23:08:41 +0400

    Return Scanner

--- src/homework_3/HomeWork.java
+++ src/homework_3/HomeWork.java
@@ -1,5 +1,7 @@
 package homework_3;
 
+import java.util.Scanner;
+
 public class HomeWork {
     public static void main(String[] args){
         // Method calls
@@ -7,7 +9,12 @@ public class HomeWork {
         // После этого подгружаете все, как я вам и показывал
         // В ТГ в группе ДЗ ставите + с указанием номера домашки
         // Например, ДЗ3 +, ДЗ4 +
+        String name = input().nextLine();
     }
 
     // Одно задание - один метод
+
+    public static Scanner input(){
+        return new Scanner(System.in);
+    }
 }
